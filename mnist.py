7d0a8a39ed4cd4265bf4d723aeae2b7284a46053
Daniel Verdejo <dverdagio@gmail.com>
2017-09-28 13:32:23 +0100

    changed mnist.py - now reads and returns magic number & number of images

--- mnist.py
+++ mnist.py
@@ -3,12 +3,26 @@
 #   https://stackoverflow.com/questions/2872381/how-to-read-a-file-byte-by-byte-in-python-and-how-to-print-a-bytelist-as-a-binar
 import gzip
 
+def read_labels_from_file(filename) :
+    with gzip.open(filename, 'rb') as f:
+        magic = f.read(4)
+        magic = int.from_bytes(magic, 'big')
+        print("magic is: ", magic)
+        
+        nolab = f.read(4)
+        nolab = int.from_bytes(nolab, 'big')
+        print("no of labels: ", nolab)
 
-f = gzip.open('data/train-images-idx3-ubyte.gz', 'rb')
+        labels = [f.read(1) for i in range(nolab)]
+        labels = [int.from_bytes(label, 'big') for label in labels]
+        
+        return labels
+    
+train_labesl = read_labels_from_file('data/train-images-idx3-ubyte.gz')
+test_labesl = read_labels_from_file('data/t10k-images-idx3-ubyte.gz')
+        
 
-i = f.read(4)
-print(i)
-
-print(int.from_bytes(i, byteorder='big'))
-
+    
+    
+    
 f.close()
