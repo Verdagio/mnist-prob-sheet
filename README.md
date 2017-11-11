e21349705a7ee8897ac7ddf3ff90e081f1ac62a1
Verdagio <dverdagio@gmail.com>
2017-11-11 17:56:03 +0000

    updated read me & added comments to mnist.py

--- README.md
+++ README.md
@@ -1,5 +1,42 @@
 # mnist-prob-sheet
 
+# Introduction
+The following is an in class assignment of: Reading in the MNIST data sets byte by byte into a data structure to then output the image to console & save it as a .PNG file with a generated name.
+This machine learning exercise is to be done using the python programming language, for module Emerging technologies. 
+
+### What is the MNIST data set?
+
+The MNIST database of handwritten digits, available from this page, has a training set of 60,000 examples, and a test set of 10,000 examples. It is a subset of a larger set available from NIST. The digits have been size-normalized and centered in a fixed-size image.
+It is a good database for people who want to try learning techniques and pattern recognition methods on real-world data while spending minimal efforts on preprocessing and formatting.
+
+For more info see : [MNIST](http://yann.lecun.com/exdb/mnist/)
+
+## Getting started
+
+To be able to run these scripts we have a bit of setup to do first. Firstly we will need to have python installed you can get it here:
+
+[Python](https://www.python.org/downloads/)
+
+Once the download & installation of Python is finished we are ready to go!
+
+You might need to install some dependencies.
+
+Open a CMD / Terminal window on your system and type in the following:
+
+1. Pillow -> ```pip install pillow```
+2. Numpy -> ```pip install numpy```
+3. Gzip -> ```pip install gzip```
+
+With that out of the way we can go to the directory of the project
+
+```cd path/to/directory```
+
+Once in the project directory we will use the following command to run the a script
+
+```python mnist.py```
+
+Note.. this will generate approximately 69,000 .png images in the image/ directory 
+
 ### Asignment specification
 
 These problems relate to the famous MNIST data set. Save your work as a Python file, or a collection of Python files. Place them in a single repository on GitHub, complete with a README. The files are in a bespoke format, as described on the website.
--- mnist.py
+++ mnist.py
@@ -3,25 +3,25 @@
 #   https://stackoverflow.com/questions/2872381/how-to-read-a-file-byte-by-byte-in-python-and-how-to-print-a-bytelist-as-a-binar
 import gzip
 import numpy as np
-import PIL.Image as pil
+import PIL.Image as pil                                       # import 3 modules required 
 
-def read_labels_from_file(filename) :
-    with gzip.open(filename, 'rb') as f:
-        magic = f.read(4)
-        magic = int.from_bytes(magic, 'big')
+def read_labels_from_file(filename) :                         # create a function that will read the labels from a .gz file 
+    with gzip.open(filename, 'rb') as f:                      # using gzip decompress the file, read it and save it as f - https://docs.python.org/2/library/gzip.html
+        magic = f.read(4)                                     # the magic number is the 4th element - http://yann.lecun.com/exdb/mnist/
+        magic = int.from_bytes(magic, 'big')                  # convert to an int from bytes using the magic number & byteorder set to big If byteorder is "big", the most significant byte is at the beginning of the byte array.
         print("magic is: ", magic)
         
-        nolab = f.read(4)
+        nolab = f.read(4)                                     # number of labels is the next piece of data to take in
         nolab = int.from_bytes(nolab, 'big')
         print("number of labels: ", nolab)
 
-        labels = [f.read(1) for i in range(nolab)]
-        labels = [int.from_bytes(label, 'big') for label in labels]
+        labels = [f.read(1) for i in range(nolab)]            # create an array of labels based off the size
+        labels = [int.from_bytes(label, 'big') for label in labels]     # and insert a label into each
         
         return labels
 
 
-def read_images_from_file(file):
+def read_images_from_file(file):                              # again we do much the same as before except this time we will get the magic number, labels, number of rows & columns
     with gzip.open(file, 'rb') as f:
         magic = f.read(4)
         magic = int.from_bytes(magic, 'big')
@@ -40,27 +40,27 @@ def read_images_from_file(file):
         print("number of cols: ", nocol)    
         
         
-        images = []
-        
-        for i in range(nolab):
-            rows = []
+        images = []                                          
+
+        for i in range(nolab):                              # in this nested loop we will populate the rows & columns with data 
+            rows = []                                         
             for r in range(norow):
                 cols = []
                 for c in range(nocol):
-                    cols.append(int.from_bytes(f.read(1), 'big'))
-                rows.append(cols)
-            images.append(rows)
+                    cols.append(int.from_bytes(f.read(1), 'big'))   # columns store the data
+                rows.append(cols)                                   # rows store the column data
+            images.append(rows)                                     # and images store the rows (all of data essentially)
             
         return images
 
-def save(image, name, id, label):
+def save(image, name, id, label):                                   # this function will save our  image as a .png file 
     location = "images/%s-%05d-%d.png"
     img = np.array(image)
-    img = pil.fromarray(img)
-    img = img.convert('RGB')
-    img.save(location % (name, id, label))
+    img = pil.fromarray(img)                                        # Creates an image memory from an object exporting the array interface (using the buffer protocol). - http://pillow.readthedocs.io/en/3.1.x/reference/Image.html
+    img = img.convert('RGB')                                        # Returns a converted copy of this image. 
+    img.save(location % (name, id, label))                          # saves to the destination specified
 
-train_labels = read_labels_from_file('data/train-labels-idx1-ubyte.gz')
+train_labels = read_labels_from_file('data/train-labels-idx1-ubyte.gz') # call the methods to extract the data from them and store said data
 test_labels = read_labels_from_file('data/t10k-labels-idx1-ubyte.gz')
             
 train_images = read_images_from_file('data/train-images-idx3-ubyte.gz')
@@ -69,7 +69,7 @@ test_images = read_images_from_file('data/t10k-images-idx3-ubyte.gz')
 #will print out an image 
 for row in train_images[4999]:
     for col in row:
-        print('.' if col <= 127 else '#', end='')
+        print('.' if col <= 127 else '#', end='')                       # to print, we'll represent the vaule stored with either a . or # to show in console
     print()
     
 for i in range(len(train_images)):  #   Please note: this will create 60,000 png images
